180f6438ab21aa159a982948e48337f6f8264d09
Erickkirui <erickkirui653@gmail.com>
2024-05-06 13:22:13 +0300

    basic style for card

--- hacks-frontend/src/Components/Singlehack.js
+++ hacks-frontend/src/Components/Singlehack.js
@@ -18,14 +18,28 @@ const Singlehack = () => {
   }, []);
 
   return (
-    <div>
+    <div className='cheat-container'>
       {cheats.map((cheat) => (
-        <div key={cheat.id} className="card">
-          <h3>{cheat.hack}</h3>
-          <p>Username: {cheat.username}</p>
-          <p>Likes: {cheat.likes}</p>
-          <p>Dislikes: {cheat.dislikes}</p>
-          <p>Reports: {cheat.reports}</p>
+        <div key={cheat.id} className="cheat-card">
+            <div className='card-container'>
+                <div>
+                    <h3>{cheat.hack}</h3>
+                    <div className='cheat-info'>
+                        <p>Username: {cheat.username} | </p>
+                        <p>Likes: {cheat.likes} | </p>
+                        <p>Dislikes: {cheat.dislikes} | </p>
+                        <p>Reports: {cheat.reports} </p>
+
+                    </div>
+                </div>
+                <div className='card-actions-side'>
+                    <button type='submit'> Like</button>
+                    <button type='submit'>Dislike</button>
+                    <button type='submit'>Report</button>
+                
+                </div>
+            </div>
+  
         </div>
       ))}
     </div>
