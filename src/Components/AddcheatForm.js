2fba4b507a0930dc93ef65d095373515c93096a6
Erickkirui <erickkirui653@gmail.com>
2024-05-09 16:24:30 +0300

    fixes live test

--- src/Components/AddcheatForm.js
+++ src/Components/AddcheatForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { ChevronLeft, } from 'lucide-react';
+import { ChevronLeft } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 function AddCheatForm() {
@@ -12,27 +12,32 @@ function AddCheatForm() {
     event.preventDefault();
 
     try {
-      const response = await axios.post('/api/addcheat', { hack, username });
+      // Make the API request to add the cheat
+      await axios.post('/api/addcheat', { hack, username });
+      
+      // Set success message
       setMessage(`Your clever idea has been spread like party confetti, making mischief everywhere! 🎉🎊`);
+      
       // Clear form fields after successful submission
       setHack('');
       setUsername('');
     } catch (error) {
-      setMessage(`You did something wrong the site works perfectly (try a longer name)`);
+      // Set error message
+      setMessage(`You did something wrong! But hey, the site works perfectly. Maybe try a longer name?`);
     }
   };
 
   return (
     <div className='post-form-container'>
       <p><ChevronLeft /> <Link to="/">Back</Link></p>
-      <h1>What that One hack in life that helps daily?</h1>
-      <p> Make sure you follow all rules and regulation befor posting a hack. <span>No login required</span></p>
+      <h1>What's that one hack in life that helps daily?</h1>
+      <p> Make sure you follow all rules and regulations before posting a hack. <span>No login required</span></p>
       <div className='widom'>
-       {message && <p>{message}</p>}
+        {message && <p>{message}</p>}
       </div>
       
       <form onSubmit={handleSubmit}>
-        <label htmlFor="hack">Hack :</label><br />
+        <label htmlFor="hack">Hack:</label><br />
         <textarea id="hack" name="hack" value={hack} onChange={(e) => setHack(e.target.value)} /><br />
         <label htmlFor="username">Username:</label><br />
         <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} /><br /><br />
